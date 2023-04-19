ca2843d8561da4e4e5b4e2713147dfe853a876c0
fakamatony <irn21bru.official@yandex.ru>
2023-04-19 16:26:51 +0300

    refactor

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-function base(){
-  let wario = Math.floor(Math.random() * (9999 - 1111)) + 1111;
-  let stringWario = wario.toString()
-  let splitWario = stringWario.split('')
-  let a = splitWario[0]
-  let b = splitWario[1]
-  let c = splitWario[2]
-  let d = splitWario[3]
-  let oneResult = a + b + c + d
-  let twoResult = c + a + d + b
-  let threeResult = d + c + b + a
-  let fourResult = b + d + a + c
-  if (a && b === c || b && c === d || c && d === b || c && b === a){
-    console.log('kal')
-    return base()
+function base() {
+  let isUnique = false;
+  while (!isUnique) {
+    let wario = ~~(Math.random() * (9999 - 1111 + 1)) + 1111;
+    let [a, b, c, d] = [...wario+''];
+    let [r1, r2, r3, r4] = [a + b + c + d, c + a + d + b, d + c + b + a, b + d + a + c];
+    if (/([abcd]).*\1/.test(r1+r2+r3+r4)) {
+      console.log('kal');
+    } else {
+      console.log(`${r1} ${r2} ${r3} ${r4}`);
+      isUnique = true;
+    }
   }
-  console.log(oneResult +' '+ twoResult +' '+ threeResult +' '+  fourResult)
 }
-base()
-
+base();
 
 
 
